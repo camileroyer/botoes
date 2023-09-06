26d8a5d9026c0427c18bdd27f0f721bdf721dbc6
camile <85092924004@senacrs.edu.br>
2023-09-06 10:48:31 -0300

    commit2

---
+++ script.js
@@ -0,0 +1,50 @@
+//obrigatório
+$(document).ready(function(){
+    $("#ch").click(function(){
+        alert("Filosofia, geografia, história e sociologia: concentração de terras no Brasil.")
+    });
+
+    $("#pv").click(function(){
+        alert("Projeto mini-aulas.")
+    });
+
+    $("#pp").click(function(){
+        alert("Feira de ciências.")
+    });
+
+    $("#qm").click(function(){
+        alert("Interações intermoleculares e ligações atômicas.")
+    });
+
+    $("#fc").click(function(){
+        alert("Mecânica e função horária.")
+    });
+
+    $("#bg").click(function(){
+        alert("Tipos de células.")
+    });
+
+    $("#ar").click(function(){
+        alert("Gravação do curta-metragem.")
+    });
+
+    $("#lp").click(function(){
+        alert("Gêneros textuais e literários.")
+    });
+
+    $("#li").click(function(){
+        alert("Tecnologia e futuro.")
+    });
+
+    $("#ef").click(function(){
+        alert("Esportes de rebater.")
+    });
+
+    $("#tc").click(function(){
+        alert("Javascript e Boostrap.")
+    });
+
+    $("#mt").click(function(){
+        alert("Logaritmo e função exponencial.")
+    });
+})
